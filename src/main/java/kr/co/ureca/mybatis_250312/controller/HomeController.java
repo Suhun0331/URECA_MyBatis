4d3c5739c09aafdb2880ca101ed970c9ad379550
Suhun0331 <rlatngns6046@naver.com>
2025-03-12 16:18:34 +0900

    Jsp setting

---
+++ src/main/java/kr/co/ureca/mybatis_250312/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kr.co.ureca.mybatis_250312.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "/hello";
+    }
+}
